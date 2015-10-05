7bd66dbed0b4a1dcb336d664a117dfb6b5e2a0b0
Ilya Khaprov <dead.trickster@gmail.com>
2015-10-05 05:42:27 -0700

    mandatory messages example

---
+++ examples/guides/exchanges/mandatory-messages.lisp
@@ -0,0 +1,26 @@
+(in-package :cl-bunny.examples)
+
+(cl-interpol:enable-interpol-syntax)
+
+(defun mandatory-messages ()
+  (log:info "=> Publishing messages as mandatory")
+
+  (with-connection ("amqp://" :one-shot t)
+    (with-channel ()
+      (let* ((x (default-exchange))
+             (q (amqp-queue-declare "" :exclusive t)))
+
+        (setf (exchange-on-return-callback x)
+              (lambda (returned-message)
+                (log:info "Got returned message ~a" (babel:octets-to-string
+                                                     (message-body returned-message)))))
+
+        (subscribe q (lambda (message)                       
+                       (log:info "~a received ~a" q (babel:octets-to-string (message-body message)))))
+
+        (publish x "This will NOT be returned" :mandatory t :routing-key q)
+        (publish x "This will be returned" :mandatory t :routing-key #?"wefvvtrw${(random 10)}")        
+
+        (log:info "Waiting...")
+        (sleep 3)
+        (log:info "Disconnecting")))))
