a54cd56b314661fb841c18ee26be4fbb5522798b
Ilya Khaprov <dead.trickster@gmail.com>
2015-10-05 12:13:57 -0700

    hello world example

--- examples/guides/getting-started/hello-world.lisp
+++ examples/guides/getting-started/hello-world.lisp
@@ -6,7 +6,7 @@
       (amqp-queue-declare "test-queue")
       (amqp-basic-publish message :routing-key "test-queue"))))
 
-(defun test-recv ()
+(defun test-recv-sync ()
   (with-connection "amqp://"
     (with-channel ()
       (amqp-queue-declare "test-queue")
@@ -14,5 +14,33 @@
           (("test-queue"
             (lambda (message)
               (format t "Got message ~a" (babel:octets-to-string (message-body message))))
-             :type :sync))
+            :type :sync))
         (consume :one-shot t)))))
+
+(defun hello-world ()
+  (with-connection ("amqp://" :one-shot t)
+    (with-channel ()
+      (let ((x (default-exchange)))
+        (->
+          (queue.declare "cl-bunny.examples.hello-world" :auto-delete t)
+          (subscribe (lambda (message)
+                       (log:info "Received ~a"
+                                 (babel:octets-to-string (message-body message))))))
+        (publish x "Hello world!" :routing-key "cl-bunny.examples.hello-world"))
+      (sleep 1))))
+
+(defun hello-world-sync ()
+  (with-connection ("amqp://" :one-shot t)
+    (with-channel ()
+      (let ((x (default-exchange))
+            (q "cl-bunny.examples.hello-world"))
+
+        (queue.declare q :auto-delete t)
+        (with-consumers
+            ((q
+              (lambda (message)
+                (log:info "Received ~a"
+                          (babel:octets-to-string (message-body message))))
+              :type :sync))
+          (publish x "Hello world!" :routing-key q)
+          (consume :one-shot t))))))
