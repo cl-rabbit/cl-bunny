0c3299958b21a9ca2a746914245bed3c14a44008
Ilya Khaprov <dead.trickster@gmail.com>
2015-10-05 18:11:33 -0700

    export more symbols

--- package.lisp
+++ package.lisp
@@ -8,11 +8,13 @@
            #:message-channel
            #:message-consumer-tag
            #:message-delivery-tag
-           #:message-redelivered
+           #:message-redelivered-p
            #:message-exchange
            #:message-routing-key
            #:message-properties
            #:message-body
+           #:message-ack
+           #:message-header-value
 
            #:amqp-queue-declare
            #:amqp-queue-bind
@@ -25,14 +27,27 @@
            #:amqp-basic-consume
 
            ;; high-level interfaces
+           #:properties-headers
+           #:header-value
+           
+           #:new-connection
+           #:connection-start
+           #:connection-close
+
+           #:new-channel
+           #:channel-open
+           #:channel-prefetch
+           
            #:exchange-on-return-callback
 
            #:queue.declare
            #:queue.bind
+           #:queue.purge
            
            #:default-exchange
            #:topic-exchange
            #:fanout-exchange
+           #:direct-exchange
            #:publish          
            #:consume
            #:subscribe))
