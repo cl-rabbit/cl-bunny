061141569f7931c71c0e55777e527e0ee5d224ad
Ilya Khaprov <dead.trickster@gmail.com>
2015-10-14 14:00:30 +0300

    move deps install to separate script

---
+++ install-travis-deps.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+#first clone Quicklisp stuff
+git clone https://github.com/deadtrickster/safe-queue.git /home/travis/.roswell/impls/ALL/ALL/quicklisp/local-projects/safe-queue
+git clone https://github.com/deadtrickster/eventfd.git /home/travis/.roswell/impls/ALL/ALL/quicklisp/local-projects/eventfd
+git clone https://github.com/fukamachi/quri.git /home/travis/.roswell/impls/ALL/ALL/quicklisp/local-projects/quri
+git clone -b master1 https://github.com/deadtrickster/cl-rabbit.git /home/travis/.roswell/impls/ALL/ALL/quicklisp/local-projects/cl-rabbit
+
+#clone and build latest librabbitmq
+git clone https://github.com/alanxz/rabbitmq-c.git ~/rabbitmq-c
+mkdir ~/rabbitmq-c/build
+cd ~/rabbitmq-c/build
+cmake ..
+cmake --build . --target install
