8c989fb66b01ab8b44505eef46bd3a5906e4ab14
Ilya Khaprov <dead.trickster@gmail.com>
2015-10-30 21:40:55 +0300

    accept queue as routing key

--- src/connection/exchange.lisp
+++ src/connection/exchange.lisp
@@ -17,11 +17,17 @@
 (defmethod exchange-channel ((exchange string))
   nil)
 
+(defmethod routing-key ((routing-key string))
+  routing-key)
+
+(defmethod routing-key ((routing-key queue))
+  (queue-name routing-key))
+
 (defun publish (exchange payload &key routing-key mandatory immediate properties
                                   (encoding :utf-8)
                                   (channel *channel*))
   (amqp-basic-publish payload :exchange (exchange-name exchange)
-                              :routing-key routing-key
+                              :routing-key (routing-key routing-key)
                               :mandatory mandatory
                               :immediate immediate
                               :properties properties
