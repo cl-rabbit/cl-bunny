2bfacf92aa167ec11f96e05bd3c123dd2b7417af
Ilya Khaprov <dead.trickster@gmail.com>
2015-10-31 12:48:30 +0300

    move exchange related stuffto exchange.lisp

--- src/connection/channel.lisp
+++ src/connection/channel.lisp
@@ -46,51 +46,9 @@
 (defun (setf channel-prefetch) (value channel &key global)
   (amqp-basic-qos value :global global :channel channel))
 
-
 (defun channel-consume-message (channel message &key return)
   (if-let ((consumer (find-message-consumer channel message)))
     (if (eq :sync (consumer-type consumer))
         (mailbox-send-message (channel-mailbox channel) message)
         (execute-consumer consumer message))
     (log:error "Unknown consumer tag ~a." (message-consumer-tag message))))
-
-(defun get-registered-exchange (channel name)
-  (gethash name (channel-exchanges channel)))
-
-(defun register-exchange (channel exchange)
-  (setf (gethash (exchange-name exchange) (channel-exchanges channel))
-        exchange))
-
-(defun default-exchange (&optional (channel *channel*))
-  (or
-   (get-registered-exchange channel "")
-   (register-exchange channel (make-instance 'exchange :channel channel
-                                                       :name ""))))
-
-(defun direct-exchange (name &rest args &key passive durable auto-delete internal arguments (channel *channel*))
-  (apply #'amqp-exchange-declare
-         (append (list  name
-                        :type "direct")
-                 args))
-  (make-instance 'exchange :name name :channel channel))
-
-(defun topic-exchange (name &rest args &key passive durable auto-delete internal arguments (channel *channel*))
-  (apply #'amqp-exchange-declare
-         (append (list  name
-                        :type "topic")
-                 args))
-  (make-instance 'exchange :name name :channel channel))
-
-(defun fanout-exchange (name &rest args &key passive durable auto-delete internal arguments (channel *channel*))
-  (apply #'amqp-exchange-declare
-         (append (list  name
-                        :type "fanout")
-                 args))
-  (make-instance 'exchange :name name :channel channel))
-
-(defun headers-exchange (name &rest args &key passive durable auto-delete internal arguments (channel *channel*))
-  (apply #'amqp-exchange-declare
-         (append (list  name
-                        :type "headers")
-                 args))
-  (make-instance 'exchange :name name :channel channel))
--- src/connection/exchange.lisp
+++ src/connection/exchange.lisp
@@ -17,6 +17,47 @@
 (defmethod exchange-channel ((exchange string))
   nil)
 
+(defun get-registered-exchange (channel name)
+  (gethash name (channel-exchanges channel)))
+
+(defun register-exchange (channel exchange)
+  (setf (gethash (exchange-name exchange) (channel-exchanges channel))
+        exchange))
+
+(defun default-exchange (&optional (channel *channel*))
+  (or
+   (get-registered-exchange channel "")
+   (register-exchange channel (make-instance 'exchange :channel channel
+                                                       :name ""))))
+
+(defun direct-exchange (name &rest args &key passive durable auto-delete internal arguments (channel *channel*))
+  (apply #'amqp-exchange-declare
+         (append (list  name
+                        :type "direct")
+                 args))
+  (make-instance 'exchange :name name :channel channel))
+
+(defun topic-exchange (name &rest args &key passive durable auto-delete internal arguments (channel *channel*))
+  (apply #'amqp-exchange-declare
+         (append (list  name
+                        :type "topic")
+                 args))
+  (make-instance 'exchange :name name :channel channel))
+
+(defun fanout-exchange (name &rest args &key passive durable auto-delete internal arguments (channel *channel*))
+  (apply #'amqp-exchange-declare
+         (append (list  name
+                        :type "fanout")
+                 args))
+  (make-instance 'exchange :name name :channel channel))
+
+(defun headers-exchange (name &rest args &key passive durable auto-delete internal arguments (channel *channel*))
+  (apply #'amqp-exchange-declare
+         (append (list  name
+                        :type "headers")
+                 args))
+  (make-instance 'exchange :name name :channel channel))
+
 (defmethod routing-key ((routing-key string))
   routing-key)
 
