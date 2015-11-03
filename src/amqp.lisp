116ed2a94b0a6a24ed7871024c980dcefb97c62e
Ilya Khaprov <dead.trickster@gmail.com>
2015-11-03 09:17:06 -0800

    message-nack

--- src/amqp.lisp
+++ src/amqp.lisp
@@ -134,3 +134,20 @@
                        (channel-id channel)
                        delivery-tag
                        :multiple multiple))
+
+(defun amqp-basic-nack (delivery-tag &rest args &key multiple requeue (channel *channel*))
+  (remf args :channel)
+  (execute-in-connection-thread-sync ((channel-connection channel))
+    (cl-rabbit:basic-nack (connection-cl-rabbit-connection (channel-connection channel))
+                         (channel-id channel)
+                         delivery-tag
+                         :multiple multiple
+                         :requeue requeue)))
+
+(defun amqp-basic-nack-async (delivery-tag &rest args &key multiple requeue (channel *channel*))
+  (remf args :channel)
+  (cl-rabbit:basic-nack (connection-cl-rabbit-connection (channel-connection channel))
+                       (channel-id channel)
+                       delivery-tag
+                       :multiple multiple
+                         :requeue requeue))
