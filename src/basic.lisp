c6800142e862b5501781dabdae355aac8ffa6bd1
Ilya Khaprov <dead.trickster@gmail.com>
2015-11-04 23:46:24 +0300

    fix typo

--- src/basic.lisp
+++ src/basic.lisp
@@ -7,7 +7,7 @@
   (queue-name routing-key))
 
 (defun publish (exchange content &key (routing-key "") (mandatory nil) (immediate nil) (channel *channel*) (properties (make-instance 'amqp-basic-class-properties)))
-  (channel-send% (or channel (exchange-channel channel))
+  (channel-send% (or channel (exchange-channel exchange))
       (make-instance 'amqp-method-basic-publish
                      :exchange (exchange-name exchange)
                      :routing-key (routing-key routing-key)
