0393eae477e77a054fa839ceca7322fd219c57a6
Ilya Khaprov <dead.trickster@gmail.com>
2015-11-08 13:11:13 +0300

    update *logs_topic tutuorials

--- examples/tutorials/emit_log_topic.lisp
+++ examples/tutorials/emit_log_topic.lisp
@@ -7,7 +7,7 @@
     (let* ((args (cdr sb-ext:*posix-argv*))
            (severity (if (car args) (car args) "anonimous.info"))
            (msg (format nil "~{~a ~}" (cdr args)))
-           (x (amqp-exchange-declare "topic_logs" :type "topic")))
+           (x (exchange.declare "topic_logs" :type "topic")))
       (publish x msg :routing-key severity)
       (format t " [x] Sent ~a:'~a'" severity msg)
       (sleep 1))))
