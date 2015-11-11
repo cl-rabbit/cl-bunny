1184a094dccfbff1c9f4d7e7e1a64c7fed7817b6
Ilya Khaprov <dead.trickster@gmail.com>
2015-11-11 15:06:37 +0300

    add basic sync consumer timeout test, async consumer

--- t/integration/basic.lisp
+++ t/integration/basic.lisp
@@ -3,24 +3,71 @@
 (plan 1)
 
 (subtest "Basic publish/consume test"
-  (with-connection ("amqp://")
-    (let ((queue))
-      (flet ((test-send (message)
-               (with-channel ()
-                 (let ((x (exchange.default)))
-                   (setf queue (queue.declare :auto-delete t))
-                   (publish x message :routing-key queue))))
-             (test-recv-sync ()
-               (with-channel ()
-                 (with-consumers
-                     ((queue
-                       (lambda (message)
-                         (message-ack message)
-                         (return-from test-recv-sync (message-body-string message)))
-                       :type :sync))
-                   (consume :one-shot t)))))
-        (test-send "Hello World!")
-        (is (test-recv-sync) "Hello World!")))))
 
+  (subtest "Sync with-consumers"
+    (with-connection ()
+      (let ((queue))
+        (labels ((test-send (message)
+                   (with-channel ()
+                     (let ((x (exchange.default)))
+                       (setf queue (queue.declare :auto-delete t))
+                       (publish x message :routing-key queue))))
+                 (test-recv-sync ()
+                   (let ((string))
+                     (with-channel ()
+                       (with-consumers
+                           ((queue
+                             (lambda (message)
+                               (message-ack message)
+                               (setf string (message-body-string message)))
+                             :type :sync))
+                         (is (consume :one-shot t) "Hello World!" "Sync consumer didn't timed out"))
+                       string))))
+          (test-send "Hello World!")
+          (is (test-recv-sync) "Hello World!")))))
+
+  (subtest "Timed out one-shot consume"
+    (with-connection ("amqp://")
+      (with-channel ()
+        (with-consumers
+            (((queue.declare :auto-delete t)
+              (lambda (message)
+                (message-ack message)
+                (message-body-string message))
+              :type :sync))
+          (is (consume :one-shot t) nil "Sync consumer timed out")))))
+
+  (subtest "Sync with-consumers short"
+    (with-connection ()
+      (let ((queue)
+            (consumer))
+        (labels ((test-send (message)
+                   (with-channel ()
+                     (setf queue (queue.declare :auto-delete t))
+                     (publish (exchange.default) message :routing-key queue)))
+                 (test-recv-sync ()
+                   (with-channel ()
+                     (setf consumer (subscribe-sync queue))
+                     (consume :one-shot t))))
+          (test-send "Hello World!")
+          (multiple-value-bind (message ok)
+              (test-recv-sync)
+            (is ok t)
+            (is (message-body-string message) "Hello World!")
+            (is (message-consumer message) consumer "Message consumer set"))))))
+
+  (subtest "Async consumer"
+    (let ((string))
+      (with-connection ()
+        (with-channel ()
+          (let ((x (exchange.default)))
+            (subscribe
+             (queue.declare :name "cl-bunny.test.hello-world" :auto-delete t)
+             (lambda (message)
+               (setf string
+                     (message-body-string message))))
+            (publish x "Hello world!" :routing-key "cl-bunny.test.hello-world"))
+          (sleep 1)
+          (is string "Hello world!"))))))
 
 (finalize)
