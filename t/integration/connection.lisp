f57c031f1e335521fe3c5e37dccdceba95b7e8d5
Ilya Khaprov <dead.trickster@gmail.com>
2015-11-16 23:56:18 +0300

    more connection corner cases tests

--- t/integration/connection.lisp
+++ t/integration/connection.lisp
@@ -9,7 +9,7 @@
         (sb-thread:make-thread (lambda (connection)
                                  (bunny::execute-in-connection-thread (connection)
                                    (sleep 5)
-                                   (connection.close connection)))
+                                   (connection.close :connection connection)))
                                :arguments (list *connection*))
         (sleep 2)
         (is-error (queue.declare) 'bunny:threaded-promise-timeout
@@ -20,7 +20,7 @@
       (sb-thread:make-thread (lambda (connection)
                                (bunny::execute-in-connection-thread (connection)
                                  (sleep 5)
-                                 (connection.close connection)))
+                                 (connection.close :connection connection)))
                              :arguments (list *connection*))
       (sleep 1)
       (is-error (queue.declare) 'bunny:connection-closed-error
@@ -28,15 +28,54 @@
 
   (with-connection ()
     (with-channel ()
-      (let ((bunny::*force-timeout* 1))
-        (sb-thread:make-thread (lambda (connection)
-                                 (bunny::execute-in-connection-thread ((channel-connection connection))
-                                   (sleep 2)
-                                   (channel.close 201 0 0 :channel  connection)))
-                               :arguments (list *channel*))
-        (sleep 5)
-        (is-error (queue.declare) 'bunny:channel-closed-error "Can't use closed channel")
-        (connection.close *connection*))))
-  (ok "Can safely ignore closed channel and/or connection in with-channel cleanup"))
+      (queue.declare :name "qwe" :auto-delete t)
+      (with-consumers
+          (("qwe"
+            (lambda (m) (declare (ignore m)) (print "never called"))))
+        (let ((bunny::*force-timeout* 1))
+          (sb-thread:make-thread (lambda (connection)
+                                   (bunny::execute-in-connection-thread ((channel-connection connection))
+                                     (sleep 2)
+                                     (channel.close 201 0 0 :channel  connection)))
+                                 :arguments (list *channel*))
+          (sleep 5)
+          (is-error (queue.declare) 'bunny:channel-closed-error "Can't use closed channel")
+          (connection.close)))))
+  (ok "Can safely ignore closed channel and/or connection in with-channel/consumers cleanup")
+
+
+  (with-connection ()
+    (with-channel ()
+      (queue.declare :name "qwe" :auto-delete t)
+      (with-consumers
+          (("qwe"
+            (lambda (m) (declare (ignore m)) (print "never called"))))
+        (let ((bunny::*force-timeout* 1))
+          (sb-thread:make-thread (lambda (channel)
+                                   (bunny::execute-in-connection-thread ((channel-connection channel))
+                                     (with-channel channel
+                                       (sleep 10)
+                                       (connection.close :connection (channel-connection channel)))))
+                                 :arguments (list *channel*))
+          (sleep 5)
+          (connection.close)))))
+  (ok "Can safely abort stalled connection")
+
+  (with-connection ()
+    (with-channel ()
+      (queue.declare :name "qwe" :auto-delete t)
+      (with-consumers
+          (("qwe"
+            (lambda (m) (declare (ignore m)) (print "never called"))))
+        (let ((bunny::*force-timeout* 1))
+          (sb-thread:make-thread (lambda (channel)
+                                   (bunny::execute-in-connection-thread ((channel-connection channel))
+                                     (with-channel channel
+                                       (sleep 2)
+                                       (connection.close :connection (channel-connection channel)))))
+                                 :arguments (list *channel*))
+          (sleep 5)
+          (connection.close)))))
+  (ok "Do not block when closing closed connection"))
 
 (finalize)
