cf005b1f1a40c074bf99a77d54467f878453a3cf
Ilya Khaprov <dead.trickster@gmail.com>
2015-11-20 12:19:02 +0300

    test channel errors

--- t/integration/errors.lisp
+++ t/integration/errors.lisp
@@ -10,4 +10,24 @@
       (iolib.syscalls:close (cl-rabbit::get-sockfd (slot-value bunny:*connection* 'bunny::cl-rabbit-connection)))
       (is-error (queue.declare) 'bunny:network-error "Network error raised when operation performed on closed socket"))))
 
+(subtest "Channel errors"
+  (subtest "Sync channel error"
+    (with-connection ()
+      (let ((error))
+        (with-channel (:on-error (lambda (e)
+                                   (setf error e)))
+          (ignore-errors (queue.bind "xwewf" "x")))
+        (is-type error 'amqp:amqp-error-not-found))))
+
+  (subtest "Async channel error"
+    (with-connection ()
+      (with-channel ()
+        (let ((error))
+          (setf (channel-on-error-callback)
+                (lambda (e)
+                  (setf error e)))
+          (message.ack 12312)
+          (sleep 1)
+          (is-type error 'amqp:amqp-error-precondition-failed))))))
+
 (finalize)
