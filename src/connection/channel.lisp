d30a1d589919456225e321062da803e04ae8e6d2
Ilya Khaprov <dead.trickster@gmail.com>
2015-11-20 17:31:12 +0300

    +max-channels+ -> +channel-max+

--- src/connection/channel.lisp
+++ src/connection/channel.lisp
@@ -2,7 +2,7 @@
 
 (defvar *channel* nil
   "Current AMQP channel")
-(defconstant +max-channels+ 0)
+(defconstant +channel-max+ 0)
 
 (deftype channel-mode ()
   `(and symbol (member :default :transactional :consume)))
