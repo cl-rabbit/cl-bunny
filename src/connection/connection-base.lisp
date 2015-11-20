b19062637119c33cba7e3ec627c6313b13c6fefc
Ilya Khaprov <dead.trickster@gmail.com>
2015-11-20 17:31:57 +0300

    connection-server-properties, defer channel-id-allocator creating until tuned

--- src/connection/connection-base.lisp
+++ src/connection/connection-base.lisp
@@ -7,16 +7,18 @@
 
 (defconstant +frame-max+ 0)
 
+(defconstant +heartbeat-interval+ 60)
+
 (defclass connection ()
   ((spec :initarg :spec :reader connection-spec)
    (channel-id-allocator :type channel-id-allocator
-                         :initform (new-channel-id-allocator +max-channels+)
                          :reader connection-channel-id-allocator)
    (channels :type hash-table
              :initform (make-hash-table :synchronized t)
              :reader connection-channels)
    (pool-tag :initarg :pool-tag :accessor connection-pool-tag)
    (pool :initform nil :accessor connection-pool)
+   (heartbeat :initform +heartbeat-interval+ :initarg :heartbeat)
    (state :initform :closed :reader connection-state)))
 
 (defgeneric connection-channel-max% (connection))
@@ -24,6 +26,8 @@
 (defgeneric connection-frame-max% (connection))
 
 (defgeneric connection-heartbeat% (connection))
+
+(defgeneric connection-server-properties% (connection))
 
 (defun connection-channel-max (&optional (connection *connection*))
   (connection-channel-max% connection))
@@ -33,3 +37,6 @@
 
 (defun connection-heartbeat (&optional (connection *connection*))
   (connection-heartbeat% connection))
+
+(defun connection-server-properties (&optional (connection *connection*))
+  (connection-server-properties% connection))
