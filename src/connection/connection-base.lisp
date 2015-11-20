d85c0cd95b71b81478e6049d0923f420da1533e3
Ilya Khaprov <dead.trickster@gmail.com>
2015-11-20 17:02:43 +0300

    connection-[channel-max|frame-max|heartbeat]

--- src/connection/connection-base.lisp
+++ src/connection/connection-base.lisp
@@ -5,6 +5,8 @@
 
 (defparameter *connection-type* 'librabbitmq-connection)
 
+(defconstant +frame-max+ 0)
+
 (defclass connection ()
   ((spec :initarg :spec :reader connection-spec)
    (channel-id-allocator :type channel-id-allocator
@@ -16,3 +18,18 @@
    (pool-tag :initarg :pool-tag :accessor connection-pool-tag)
    (pool :initform nil :accessor connection-pool)
    (state :initform :closed :reader connection-state)))
+
+(defgeneric connection-channel-max% (connection))
+
+(defgeneric connection-frame-max% (connection))
+
+(defgeneric connection-heartbeat% (connection))
+
+(defun connection-channel-max (&optional (connection *connection*))
+  (connection-channel-max% connection))
+
+(defun connection-frame-max (&optional (connection *connection*))
+  (connection-frame-max% connection))
+
+(defun connection-heartbeat (&optional (connection *connection*))
+  (connection-heartbeat% connection))
