29c48c9c5835748dcceb5a961ab48803003f7bc3
Ilya Khaprov <dead.trickster@gmail.com>
2015-11-24 03:58:42 +0300

    fix exchange.unbind

--- src/exchange.lisp
+++ src/exchange.lisp
@@ -127,8 +127,8 @@
 (defun exchange.unbind (destination source &key (routing-key "") (nowait nil) (arguments nil) (channel *channel*))
   (channel.send% channel
       (make-instance 'amqp-method-exchange-unbind
-                     :destination destination
-                     :source source
+                     :destination (exchange-name destination)
+                     :source (exchange-name source)
                      :routing-key routing-key
                      :nowait nowait
                      :arguments arguments)
