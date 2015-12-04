f7507590109e8696d3fad93671d929a4c2abe97a
Ilya Khaprov <dead.trickster@gmail.com>
2015-12-04 14:10:34 +0300

    test new connection-spec stuff

--- t/unit/connection-spec.lisp
+++ t/unit/connection-spec.lisp
@@ -2,6 +2,19 @@
 
 (plan 2)
 
+(subtest "Parser helpers"
+  (subtest "check-unit-paramter"
+    (is-error (bunny::check-uint-parameter '(("frame-max" . "qwe")) "frame-max") 'error) ;; TODO: specialize error
+    (is-error (bunny::check-uint-parameter '(("frame-max" . "-2")) "frame-max") 'error) ;; TODO: specialize error
+    (is (bunny::check-uint-parameter '(("frame-max" . "256")) "frame-max") 256))
+
+  (subtest "check-connection-parameters"
+    (is-error (bunny::check-connection-parameters "frame-max=wer") 'error)
+    (is-error (bunny::check-connection-parameters "frame-max=-2") 'error)
+    (is-values (bunny::check-connection-parameters "frame-max=256") '(#.bunny::+channel-max+ 256 #.bunny::+heartbeat-interval+))
+    (is-values (bunny::check-connection-parameters "qwe=qwe&frame-max=256") '(#.bunny::+channel-max+ 256 #.bunny::+heartbeat-interval+))
+    (is-values (bunny::check-connection-parameters "heartbeat-interval=34&frame-max=256&channel-max=0") '(0 256 34))))
+
 (subtest "Connection string parser tests [without additional params]"
   ;; this should comply with https://www.rabbitmq.com/uri-spec.html
   ;; tests based on Apendix A
@@ -14,6 +27,9 @@
       (is (connection-spec-vhost spec) "vhost")
       (is (connection-spec-use-tls-p spec) nil "Do not use TLS")
       (is (connection-spec-use-ipv6-p spec) nil "Address is not IPv6")
+      (is (connection-spec-channel-max spec) bunny::+channel-max+ "Channel max is default")
+      (is (connection-spec-frame-max spec) bunny::+frame-max+ "Frame max is default")
+      (is (connection-spec-heartbeat-interval spec) bunny::+heartbeat-interval+ "Heartbeat interval is default")
       (is (print-amqp-object-to-string spec) "amqp://user:pass@host:10000/vhost")))
 
   (subtest "amqp://user%61:%61pass@ho%61st:10000/v%2fhost"
@@ -147,6 +163,35 @@
       (is (connection-spec-use-tls-p spec) nil "Do not use TLS")
       (is (connection-spec-use-ipv6-p spec) t "Address is IPv6")
       (is (print-amqp-object-to-string spec) "amqp://[::1]"))))
+
+(subtest "Connection string parser tests [with additional params]"
+  (subtest "amqp://user:pass@host:10000/vhost?frame-max=256"
+    (let ((spec (bunny::make-connection-spec "amqp://user:pass@host:10000/vhost?frame-max=256")))
+      (is (connection-spec-login spec) "user")
+      (is (connection-spec-password spec) "pass")
+      (is (connection-spec-host spec) "host")
+      (is (connection-spec-port spec) 10000)
+      (is (connection-spec-vhost spec) "vhost")
+      (is (connection-spec-use-tls-p spec) nil "Do not use TLS")
+      (is (connection-spec-use-ipv6-p spec) nil "Address is not IPv6")
+      (is (connection-spec-channel-max spec) bunny::+channel-max+ "Channel max is default")
+      (is (connection-spec-frame-max spec) 256 "Frame max is 256")
+      (is (connection-spec-heartbeat-interval spec) bunny::+heartbeat-interval+ "Heartbeat interval is default")
+      (is (print-amqp-object-to-string spec) "amqp://user:pass@host:10000/vhost?frame-max=256")))
+  
+  (subtest "amqp://user:pass@host:10000/vhost?heartbeat-interval=60&channel-max=256"
+    (let ((spec (bunny::make-connection-spec "amqp://user:pass@host:10000/vhost?heartbeat-interval=60&channel-max=256")))
+      (is (connection-spec-login spec) "user")
+      (is (connection-spec-password spec) "pass")
+      (is (connection-spec-host spec) "host")
+      (is (connection-spec-port spec) 10000)
+      (is (connection-spec-vhost spec) "vhost")
+      (is (connection-spec-use-tls-p spec) nil "Do not use TLS")
+      (is (connection-spec-use-ipv6-p spec) nil "Address is not IPv6")
+      (is (connection-spec-channel-max spec) 256 "Channel max is 256")
+      (is (connection-spec-frame-max spec) bunny::+frame-max+ "Frame max is default")
+      (is (connection-spec-heartbeat-interval spec) 60 "Heartbeat interval is 60")
+      (is (print-amqp-object-to-string spec) "amqp://user:pass@host:10000/vhost?channel-max=256&heartbeat-interval=60"))))
 
 (subtest "Connection list parser tests [without additional params]"
   (subtest "NIL"
