749d67cb088641f25045815a40ecb9b6230b35d4
Ilya Khaprov <dead.trickster@gmail.com>
2016-01-22 15:28:25 +0300

    [t] connection-pool: with-connection :shared tag test

--- t/unit/connection-pool.lisp
+++ t/unit/connection-pool.lisp
@@ -10,7 +10,7 @@
       (connections-pool.add connection1)
       (is (bunny::connection-pool connection1) *connections-pool* "Connection pool is set")
       (is (connections-pool.get spec) connection1 "Connection added to pool")
-      (is (connections-pool.find-or-run spec) connection1 "Don't create new connection for known tag")
+      (is (connections-pool.find-or-run spec spec) connection1 "Don't create new connection for known tag")
       (connections-pool.remove connection1)
       (is (connections-pool.get spec) nil "Connection removed from pool")
       (is (bunny::connection-pool connection1) nil "Connection pool is nil")))
@@ -30,6 +30,17 @@
                (connection.open connection1)
                (is (cl-bunny.test::print-amqp-object-to-string pool) "Connections: 3, Open: 1"))
           (connection.close :connection connection1))
-        (is (cl-bunny.test::print-amqp-object-to-string pool) "Connections: 2, Open: 0")))))
+        (is (cl-bunny.test::print-amqp-object-to-string pool) "Connections: 2, Open: 0")))
+
+    (subtest "With-connection shared tag"
+      (let ((connection))
+        (unwind-protect
+             (progn
+               (with-connection ("amqp://" :shared :qwe)
+                 (setf connection *connection*))
+               (with-connection (:shared :qwe)
+                 (is connection *connection*)))
+          (when connection
+            (connection.close :connection connection)))))))
 
 (finalize)
