569c7b7fcb6bec8e9fe5a790d2def3d280128f15
Ilya Khaprov <dead.trickster@gmail.com>
2016-02-05 01:46:34 +0300

    test subscribing to non existent queue

--- t/integration/consumer.lisp
+++ t/integration/consumer.lisp
@@ -5,9 +5,14 @@
 (subtest "Sync consumer test"
   (subtest "Consume on closed channel should signal channel-closed-error"
     (with-connection ()
-        (with-channel ()
-          (channel.close 200 0 0)
-          (is-error (consume) 'channel-closed-error))))
+      (with-channel ()
+        (channel.close 200 0 0)
+        (is-error (consume) 'channel-closed-error))))
+
+  (subtest "Consume should signal amqp-error-not-found when subscribing to unknown queue"
+    (with-connection ()
+      (with-channel ()
+        (is-error (subscribe "qweqweqwe" #'identity) 'amqp:amqp-error-not-found))))
 
   (subtest "Consume should signal channel-closed-error"
     (with-connection ()
