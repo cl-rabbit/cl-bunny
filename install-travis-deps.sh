342c3fa966893a8e196832cec4c9903636acbdcb
Ilya Khaprov <dead.trickster@gmail.com>
2016-02-06 16:10:20 +0300

    remove obsolete stuff

--- install-travis-deps.sh
+++ install-travis-deps.sh
@@ -6,14 +6,5 @@ git clone https://github.com/deadtrickster/safe-queue.git /home/travis/.roswell/
 git clone https://github.com/deadtrickster/eventfd.git /home/travis/.roswell/impls/ALL/ALL/quicklisp/local-projects/eventfd
 git clone https://github.com/fukamachi/quri.git /home/travis/.roswell/impls/ALL/ALL/quicklisp/local-projects/quri
 git clone https://github.com/cl-rabbit/cl-amqp.git /home/travis/.roswell/impls/ALL/ALL/quicklisp/local-projects/cl-amqp
-git clone -b master1 https://github.com/deadtrickster/cl-rabbit.git /home/travis/.roswell/impls/ALL/ALL/quicklisp/local-projects/cl-rabbit
 git clone https://github.com/pkhuong/string-case.git /home/travis/.roswell/impls/ALL/ALL/quicklisp/local-projects/string-case
 git clone https://github.com/sionescu/bordeaux-threads.git /home/travis/.roswell/impls/ALL/ALL/quicklisp/local-projects/bordeaux-threads
-
-#clone and build latest librabbitmq
-git clone https://github.com/alanxz/rabbitmq-c.git ~/rabbitmq-c
-mkdir ~/rabbitmq-c/build
-cd ~/rabbitmq-c/build
-cmake ..
-sudo cmake --build . --target install
-sudo cp /usr/local/lib/x86_64-linux-gnu/librabbitmq.so.4.1.4 /usr/lib/librabbitmq.so
